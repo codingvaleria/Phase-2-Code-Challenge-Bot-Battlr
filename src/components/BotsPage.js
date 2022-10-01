13399fb944287dc4aba3a47536dd4893c1a4aaa0
codingvaleria <codingvaleria@gmail.com>
2022-10-01 11:29:46 +0300

    Setting profiles for Bots

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
 
 function BotsPage() {
   //start here with your code for step one
+  const [bots, setBots] = useState([]);
+
+  //Fetch Bots
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+      .then((r) => r.json())
+      .then((bots) => setBots(bots));
+  }, []);
 
   return (
     <div>
       <YourBotArmy />
-      <BotCollection />
+      <BotCollection bots={bots} />
     </div>
-  )
+  );
 }
 
 export default BotsPage;
